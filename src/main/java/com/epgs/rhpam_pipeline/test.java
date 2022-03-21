ee306ee8381a3aee3d5483755402abd8a459413b
admin <>
2022-03-21 23:38:21 +0530

    {/src/main/java/com/epgs/rhpam_pipeline/test.java}

--- src/main/java/com/epgs/rhpam_pipeline/test.java
+++ src/main/java/com/epgs/rhpam_pipeline/test.java
@@ -8,7 +8,22 @@ public class test implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
+	@org.kie.api.definition.type.Label(value = "name")
+	private java.lang.String name;
+
 	public test() {
+	}
+
+	public java.lang.String getName() {
+		return this.name;
+	}
+
+	public void setName(java.lang.String name) {
+		this.name = name;
+	}
+
+	public test(java.lang.String name) {
+		this.name = name;
 	}
 
 }
